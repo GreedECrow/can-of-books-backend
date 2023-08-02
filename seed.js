89ba5102b210748816350254ea51826101813a25
Lee Pierson <awesomehenge@gmail.com>
2023-08-02 13:48:24 +0100

    update006

--- seed.js
+++ seed.js
@@ -7,18 +7,21 @@ async function seed(){
     
     await Book.create({
         title: "The Last of Us",
+        img_url:"*",
         description: "Was Alright, they all died at the end.",
         status: "Very Sad",
     })
 
     await Book.create({
         title: "Colouring Book",
+        img_url:"*",
         description: "Care bears should not look like that.",
         status: "Difficult read.",
     })
 
     await Book.create({
         title: "Dodge Ball",
+        img_url:"*",
         description: "A cinderella Story",
         status: "Banned in 25 countries.",
     })
